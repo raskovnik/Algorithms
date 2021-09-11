3f4fd8fa8849cde5a9102899ae1f2936058d4fd9
raskovnik <4lch3my.3ck0@gmail.com>
2021-09-11 18:18:03 +0300

    merge sort in python

--- merge_sort/merge_sort.py
+++ merge_sort/merge_sort.py
@@ -1,29 +1,34 @@
-def merge_sort(sequence):
-    merging(sequence, 0, len(sequence) - 1)
+def mergesort(list):
+    if len(list) > 1:
+        mid = len(list) // 2
+        left = list[:mid]
+        right = list[mid:]
 
-def merging(sequence, first, last):
-    if first < last:
-        middle = (first + last) // 2
-        merging(sequence, first, middle)
-        merging(sequence, middle + 1, last)
-        merge(sequence, first, middle, last)
+        mergesort(left)
+        mergesort(right)
 
-def merge(sequence, first, middle, last):
-    left_half = sequence[first:middle]
-    right_half = sequence[middle:last + 1]
-    right_half.append(9999999)
-    left_half.append(9999999)
-    i = j = 0
-    for k in range(first, last + 1):
-        if left_half[i] <= right_half[i]:
-            sequence[k] = left_half[i]
-            i += 1
-
-        else:
-            sequence[k] = right_half[j]
-            j += 1
-
-    return sequence
-
-print(merge_sort([5,6,5,1,5,7,8,1,2,4,6,7,8,9,0]))
+        a, b, c = 0, 0, 0
         
+        while a < len(left) and b < len(right):
+            if left[a] < right[b]:
+                list[c] = left[a]
+                a = a + 1
+            else:
+                list[c] = right[b]
+                b = b + 1
+            c = c + 1
+
+        while a < len(left):
+            list[c] = left[a]
+            a = a + 1
+            c = c + 1
+
+        while b < len(right):
+            list[c] = right[b]
+            b = b + 1
+            c = c + 1
+
+    return list
+
+list = [3, 4, 4, 6, 3352, 345, 56, 234, 5462, 456]
+print(mergesort(list))
