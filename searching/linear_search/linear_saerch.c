fa4c5a12781ddefa5730f0fbd015e0987b995639
raskovnik <4lch3my.3ck0@gmail.com>
2021-09-14 13:01:09 +0300

    linear search in C and rust

--- searching/linear_search/linear_saerch.c
+++ searching/linear_search/linear_saerch.c
@@ -13,9 +13,15 @@ int linear_search(int list[SIZE], int item) {
     return found;
 }
 
+int my_linear_search(int list[SIZE], int item) {
+    for(int i; i < SIZE; i++) {
+        if (list[i] == item) {return 1;}
+    }
+    return 0;
+}
 int main() {
     int a[SIZE] = {1,2, 3, 4, 5,6,7};
-    int result = linear_search(a, 5);
+    int result = my_linear_search(a, 5);
     printf("%d\n", result);
 
 }
--- searching/linear_search/linear_search.rs
+++ searching/linear_search/linear_search.rs
@@ -8,8 +8,18 @@ fn linear_search(list: [i32;7], item: i32) -> bool {
     }
     return found;
 }
+
+fn linear_search2(list: [i32;7], item: i32) -> bool {
+    for c in list.iter() {
+        if *c == item {
+            return true;
+        }
+    }
+    return false;
+}
+
 fn main() {
     let list: [i32;7] = [1, 2, 3, 4, 5, 6, 7];
-    let result = linear_search(list, 5);
+    let result = linear_search2(list, 5);
     println!("{:?}", result);
 }
