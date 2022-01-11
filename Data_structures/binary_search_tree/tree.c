813c1c4b155cae1217d23002035405de624d9944
Anthony K. Mwangi <68157384+raskovnik@users.noreply.github.com>
2022-01-12 00:29:36 +0300

    binary search tree in c

---
+++ Data_structures/binary_search_tree/tree.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct treenode {
+    int value;
+    struct treenode *left;
+    struct treenode *right;
+
+} treenode;
+
+treenode *createnode(int value)
+int main() {
+
+}
